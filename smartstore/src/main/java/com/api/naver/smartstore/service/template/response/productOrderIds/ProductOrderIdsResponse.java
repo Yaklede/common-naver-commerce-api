1d72fcd14f0ee09a06efc24799025dfb6937deab
Yaklede <chh78151@naver.com>
2023-04-13 17:58:22 +0900

    feature: 주문 정보 조회 response 추가

---
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/response/productOrderIds/ProductOrderIdsResponse.java
@@ -0,0 +1,21 @@
+package com.api.naver.smartstore.service.template.response.productOrderIds;
+
+import com.api.naver.smartstore.service.template.common.NaverCommonResponse;
+import com.api.naver.smartstore.service.template.enumeration.ServiceType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class ProductOrderIdsResponse implements NaverCommonResponse {
+    @JsonProperty("timestamp")
+    private String timestamp;
+    @JsonProperty("traceId")
+    private String traceId;
+    @JsonProperty("data")
+    private String[] productOrderIds;
+    @Override
+    public ServiceType findServiceType() {
+        return ServiceType.PRODUCT_ORDER_IDS;
+    }
+
+}
