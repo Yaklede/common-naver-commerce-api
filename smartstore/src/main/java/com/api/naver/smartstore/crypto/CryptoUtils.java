dd7eff8cca0347dc87048a535c056c0b8073e16b
Yaklede <chh78151@naver.com>
2023-04-13 17:53:45 +0900

    fix: parameter NaverToken객체로 통일화

--- smartstore/src/main/java/com/api/naver/smartstore/crypto/CryptoUtils.java
+++ smartstore/src/main/java/com/api/naver/smartstore/crypto/CryptoUtils.java
@@ -1,19 +1,19 @@
 package com.api.naver.smartstore.crypto;
 
+import com.api.naver.smartstore.service.template.common.NaverToken;
 import org.apache.commons.lang3.StringUtils;
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class CryptoUtils {
-    public static String issueClientSecretSign(String clientId, String clientSecret, Long timestamp) {
+    public static String issueClientSecretSign(NaverToken token, Long timestamp) {
         //Password 생성
-        String password = StringUtils.joinWith( "_",clientId, timestamp);
+        String password = StringUtils.joinWith( "_",token.getClientId(), timestamp);
 
         //hashedPassword 생성
-        String hashedPassword = BCrypt.hashpw(password, clientSecret);
+        String hashedPassword = BCrypt.hashpw(password, token.getClientSecret());
 
         //encoding 작업
         return Base64.getUrlEncoder().encodeToString(hashedPassword.getBytes(StandardCharsets.UTF_8));
---
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/common/NaverToken.java
@@ -0,0 +1,11 @@
+package com.api.naver.smartstore.service.template.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NaverToken {
+    private String clientId;
+    private String clientSecret;
+}
