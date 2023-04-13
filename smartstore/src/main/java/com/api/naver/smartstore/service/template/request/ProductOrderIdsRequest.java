6d0a2f73d8d8bf2dcc9bc6af19a64498ea4ddcf6
Yaklede <chh78151@naver.com>
2023-04-13 17:57:54 +0900

    feature: 주문 정보조회 request 추가

---
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/request/ProductOrderIdsRequest.java
@@ -0,0 +1,39 @@
+package com.api.naver.smartstore.service.template.request;
+
+import com.api.naver.smartstore.service.template.common.NaverCommonResponse;
+import com.api.naver.smartstore.service.template.enumeration.ServiceType;
+import com.api.naver.smartstore.service.template.common.NaverCommonRequest;
+import com.api.naver.smartstore.service.template.response.productOrderIds.ProductOrderIdsResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpMethod;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ProductOrderIdsRequest implements NaverCommonRequest {
+
+    @Override
+    public ServiceType findServiceType() {
+        return ServiceType.PRODUCT_ORDER_IDS;
+    }
+
+    @Override
+    public HttpMethod findHttpMethod() {
+        return HttpMethod.GET;
+    }
+
+    @Override
+    public String findUrl() {
+        return "https://api.commerce.naver.com/external/v1/pay-order/seller/orders/"+orderId+"/product-order-ids";
+    }
+
+    @Override
+    public Class<? extends NaverCommonResponse> findResponseType() {
+        return ProductOrderIdsResponse.class;
+    }
+
+
+    private String orderId;
+}
