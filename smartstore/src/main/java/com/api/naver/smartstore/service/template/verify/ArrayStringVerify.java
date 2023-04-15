b3cd345283c54cf936c17ab5f581a4e04e417f9c
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-04-16 00:16:22 +0900

    fix: verify 추상 클래스 구현으로 변경

--- smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/ArrayStringVerify.java
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/ArrayStringVerify.java
@@ -1,34 +1,20 @@
 package com.api.naver.smartstore.service.template.verify;
 
-import com.api.naver.smartstore.service.template.common.NaverCommonRequest;
-import com.api.naver.smartstore.service.template.exception.RequestVerifyException;
-
-import java.lang.reflect.Field;
-
-public class ArrayStringVerify implements NaverRequestVerify {
+public class ArrayStringVerify extends AbstractNaverRequestVerify<String[]> {
     @Override
-    public <T extends NaverCommonRequest<?>> void verifyRequest(T request) {
-        Field[] declaredFields = request.getClass().getDeclaredFields();
-        for (Field declaredField : declaredFields) {
-            declaredField.setAccessible(true);
-            Object data;
-            try {
-                data = declaredField.get(request);
-                if (data == null) throw new RequestVerifyException();
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-            if (data instanceof String[]) {
-                String[] stringArrays = (String[]) data;
-                if (stringArrays.length == 0) {
-                    throw new RequestVerifyException();
-                }
-                for (String string : stringArrays) {
-                    if (string.isEmpty()) {
-                        throw new RequestVerifyException();
-                    }
-                }
+    public Class<String[]> settingVerifyType() {
+        return String[].class;
+    }
+    @Override
+    public Boolean verify(String[] data) {
+        if (data.length == 0) {
+           return false;
+        }
+        for (String string : data) {
+            if (string.isEmpty()) {
+                return false;
             }
         }
+        return true;
     }
 }
--- smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/StringVerify.java
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/StringVerify.java
@@ -1,27 +1,12 @@
 package com.api.naver.smartstore.service.template.verify;
 
-import com.api.naver.smartstore.service.template.common.NaverCommonRequest;
-import com.api.naver.smartstore.service.template.exception.RequestVerifyException;
-
-import java.lang.reflect.Field;
-
-public class StringVerify implements NaverRequestVerify {
+public class StringVerify extends AbstractNaverRequestVerify<String> {
     @Override
-    public <T extends NaverCommonRequest<?>> void verifyRequest(T request) {
-        Field[] declaredFields = request.getClass().getDeclaredFields();
-        for (Field declaredField : declaredFields) {
-            declaredField.setAccessible(true);
-            Object data;
-            try {
-                data = declaredField.get(request);
-                if (data == null) throw new RequestVerifyException();
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-
-            if (data instanceof String && ((String) data).isEmpty()) {
-                throw new RequestVerifyException();
-            }
-        }
+    public Class<String> settingVerifyType() {
+        return String.class;
+    }
+    @Override
+    public Boolean verify(String data) {
+        return !data.isEmpty();
     }
 }
