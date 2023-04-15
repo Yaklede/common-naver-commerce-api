6dc9f1b2012801e9425d6aa42b0360e6bebdedb3
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-04-15 18:18:55 +0900

    feature: 실패 response builder layer 작성

---
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/builder/FailResponseBuilder.java
@@ -0,0 +1,21 @@
+package com.api.naver.smartstore.service.template.builder;
+
+import com.api.naver.smartstore.service.template.common.FailResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FailResponseBuilder {
+    private final ObjectMapper objectMapper;
+
+    public FailResponseBuilder(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+    public FailResponse build(String responseMessage) {
+        try {
+            return objectMapper.readValue(responseMessage, FailResponse.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("실패 response를 만드는데 실패했습니다.");
+        }
+    }
+}
