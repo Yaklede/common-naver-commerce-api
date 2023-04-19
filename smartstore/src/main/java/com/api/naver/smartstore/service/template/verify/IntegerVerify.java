6dbcf901696886e6d0df39fe813c3657bc403349
Yaklede <chh78151@naver.com>
2023-04-19 13:05:39 +0900

    fix: 의미 없어진 setting Type 제거

--- smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/IntegerVerify.java
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/IntegerVerify.java
@@ -2,11 +2,6 @@ package com.api.naver.smartstore.service.template.verify;
 
 public class IntegerVerify extends AbstractNaverRequestVerify<Integer> {
     @Override
-    public Class<Integer> settingVerifyType() {
-        return Integer.class;
-    }
-
-    @Override
     public Boolean verify(Integer data) {
         if (data == null) return false;
         if (data >= 0) {
--- smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/StringVerify.java
+++ smartstore/src/main/java/com/api/naver/smartstore/service/template/verify/StringVerify.java
@@ -2,10 +2,6 @@ package com.api.naver.smartstore.service.template.verify;
 
 public class StringVerify extends AbstractNaverRequestVerify<String> {
     @Override
-    public Class<String> settingVerifyType() {
-        return String.class;
-    }
-    @Override
     public Boolean verify(String data) {
         return !data.isEmpty();
     }
